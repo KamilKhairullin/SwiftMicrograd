76177177151db7464035990c5acce0658f3968a4
Kamil Khairullin <kamil.khayrullin.personal@yandex.ru>
2024-12-28 20:52:34 +0300

    gitignore + micrograd framework

---
+++ Modules/Micrograd/Micrograd/Micrograd.h
@@ -0,0 +1,18 @@
+//
+//  Micrograd.h
+//  Micrograd
+//
+//  Created by Kamil on 28.12.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Micrograd.
+FOUNDATION_EXPORT double MicrogradVersionNumber;
+
+//! Project version string for Micrograd.
+FOUNDATION_EXPORT const unsigned char MicrogradVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Micrograd/PublicHeader.h>
+
+
--- Modules/Micrograd/Micrograd/Value.swift
+++ Modules/Micrograd/Micrograd/Value.swift
@@ -1,8 +1,23 @@
-//
-//  Value.swift
-//  Micrograd
-//
-//  Created by Kamil on 28.12.2024.
-//
-
 import Foundation
+
+struct Value {
+    
+    let data: Double
+    let gradient: Double
+    let label: String
+    let previous: Set<Value>
+    
+    init(
+        data: Double,
+        gradient: Double,
+        label: String,
+        previous: Set<Value>
+    ) {
+        self.data = data
+        self.gradient = gradient
+        self.label = label
+        self.previous = previous
+    }
+}
+
+extension Value: Hashable {}
